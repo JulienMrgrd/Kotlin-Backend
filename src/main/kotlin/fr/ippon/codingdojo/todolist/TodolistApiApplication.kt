d7db43f26f7610263fe3c280f6f033e347ea2221
Yann Sergent <ysergent@ippon.fr>
2018-09-03 15:04:41 +0200

    feature: add swagger

--- src/main/kotlin/fr/ippon/codingdojo/todolist/TodolistApiApplication.kt
+++ src/main/kotlin/fr/ippon/codingdojo/todolist/TodolistApiApplication.kt
@@ -2,8 +2,10 @@ package fr.ippon.codingdojo.todolist
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
+@EnableSwagger2
 class TodolistApiApplication
 
     fun main(args: Array<String>) {
